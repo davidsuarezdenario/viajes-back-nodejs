09cf7a789e97f4e0fd6cc7d6c92ee3125a5368c2
Sebastian <sebastian.gomez@denario.com.co>
2024-01-24 15:40:08 -0500

    update: reserva paso 2

--- src/routes/travelDenario.js
+++ src/routes/travelDenario.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const travelDenarioController = require('../controllers/travelDenario');
 const auth = require('../middleware/jwt');
 
-router.post('/search',auth, travelDenarioController.search);
+router.post('/search_airport',auth, travelDenarioController.search);
 router.post('/booking',auth, travelDenarioController.booking);
 router.post('/booking_one',auth, travelDenarioController.bookingStep1);
 
