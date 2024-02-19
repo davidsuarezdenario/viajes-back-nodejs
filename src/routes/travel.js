f9a2e8c393b532fbea17a0aa628bb71be8aa6200
Sebastian <sebastian.gomez@denario.com.co>
2024-02-19 13:57:24 -0500

    update: auth

--- src/routes/travel.js
+++ src/routes/travel.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const travelController = require('../controllers/travel');
 const auth = require('../middleware/jwt');
 
-router.post('/search_airport', auth, travelController.search_text);
-router.post('/search_location', auth, travelController.search_location);
+router.post('/search_airport', travelController.search_text);
+router.post('/search_location', travelController.search_location);
 router.post('/booking', auth, travelController.booking);
 router.post('/booking_one', auth, travelController.bookingStep1);
 
