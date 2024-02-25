4edf4067f1b79fd5b927ce9cf59e42ff57d8b8c7
Sebastiang29 <sebastian.gomez10@gmail.com>
2024-02-24 21:25:51 -0500

    update: paso 1 y 2

--- src/controllers/travel.js
+++ src/controllers/travel.js
@@ -25,6 +25,27 @@ exports.search_location = async (req, res) => {
         res.status(400).json({ error: true, data: 'No se recibe texto' });
     }
 }
+exports.search_subentity = async (req, res) => {
+    const data = req.body;
+    if (data.search != '' && data.search != undefined) {
+        let pathConsulta = `locations/subentity?term=${data.search}&locale=es-ES&limit=${data.limit}&sort=name&active_only=true'`;
+        if (data.location_types) { for (dato of data.location_types) { pathConsulta += `&location_types=${dato}`; } } else { pathConsulta += `&location_types=airport`; }
+        const resOk = await procesosTravel(pathConsulta, 'GET', {});
+        res.status(200).json({ error: false, data: JSON.parse(resOk) });
+    } else {
+        res.status(400).json({ error: true, data: 'No se recibe texto' });
+    }
+}
+exports.search_topdestinations = async (req, res) => {
+    const data = req.body;
+    if (data.search != '' && data.search != undefined) {
+        const pathConsulta = `locations/topdestinations?term=${data.search}&locale=es-ES&limit=${data.limit}&sort=name&active_only=true&source_popularity=searches`;
+        const resOk = await procesosTravel(pathConsulta, 'GET', {});
+        res.status(200).json({ error: false, data: JSON.parse(resOk) });
+    } else {
+        res.status(400).json({ error: true, data: 'No se recibe texto' });
+    }
+}
 exports.booking = async (req, res) => {
     const arreglo = Object.entries(req.body); let pathConsulta = 'v2/search?curr=COP&locale=co';
     for (dato of arreglo) { pathConsulta += `&${dato[0]}=${dato[1]}`; }
@@ -35,20 +56,29 @@ exports.bookingStep1 = async (req, res) => {
     const data = req.body;
     if (data.booking_token != '' && data.booking_token != undefined) {
         if (data.adults == undefined) { data.adults = 0; } if (data.children == undefined) { data.children = 0; } if (data.infants == undefined) { data.infants = 0; }
-        const search = `v2/booking/check_flights?booking_token=${data.booking_token}&bnum=${data.adults + data.children + data.infants}&adults=${data.adults}&children=${data.children}&infants=${data.infants}&session_id=${data.session_id}&currency=COP&visitor_uniqid=${data.visitor_uniqid}`;
+        const search = `v2/booking/check_flights?booking_token=${data.booking_token}&bnum=${data.bnum}&adults=${data.adults}&children=${data.children}&infants=${data.infants}&session_id=${data.session_id}&currency=COP&visitor_uniqid=${data.visitor_uniqid}`;
         const resOk = await procesosTravel(search, 'GET', {});
+        /* const intervalID = await setInterval(async function () {
+            resOk = await procesosTravel(search, 'GET', {});
+            console.log('resOk: ', resOk);
+            console.log(JSON.parse(resOk));
+            if (JSON.parse(resOk).flights_checked == true && JSON.parse(resOk).price_change == false && JSON.parse(resOk).flights_invalid == false) {
+                clearInterval(intervalID);
+            }
+        }, 2500); */
         res.status(200).json({ error: false, data: JSON.parse(resOk) });
     } else {
         res.status(400).json({ error: true, data: 'No se recibe texto' });
     }
 }
 exports.bookingStep2 = async (req, res) => {
-    const data = req.body; let body = { health_declaration_checked: true, lang: "es", locale: "es-CO", payment_gateway: "payu" }
+    const data = req.body; let body = { health_declaration_checked: true, lang: "es", locale: "es-CO", payment_gateway: "payu" };
+    Object.assign(body, data)
     if (data.visitor_uniqid != '' && data.visitor_uniqid != undefined) {
         if (data.adults == undefined) { data.adults = 0; } if (data.children == undefined) { data.children = 0; } if (data.infants == undefined) { data.infants = 0; }
         const search = `v2/booking/save_booking?visitor_uniqid=${data.visitor_uniqid}`;
-        const resOk = await procesosTravel(search, 'POST', {});
-        res.status(200).json({ error: false, data: JSON.parse(resOk) });
+        const resOk = await procesosTravel(search, 'POST', body);
+        res.status(200).json({ error: false, data: resOk });
     } else {
         res.status(400).json({ error: true, data: 'No se recibe texto' });
     }
--- src/routes/travel.js
+++ src/routes/travel.js
@@ -5,7 +5,10 @@ const auth = require('../middleware/jwt');
 
 router.post('/search_airport', travelController.search_text);
 router.post('/search_location', travelController.search_location);
+router.post('/search_subentity', travelController.search_subentity);
+router.post('/search_topdestinations', travelController.search_topdestinations);
 router.post('/booking', travelController.booking);
 router.post('/booking_one', travelController.bookingStep1);
+router.post('/booking_two', travelController.bookingStep2);
 
 module.exports = router;
