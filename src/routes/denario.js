d84add37d7273c06610af935c4cc7b9a5feee704
Sebastiang29 <sebastian.gomez10@gmail.com>
2024-03-30 01:01:38 -0500

    update: servicios denario

--- src/routes/denario.js
+++ src/routes/denario.js
@@ -4,6 +4,9 @@ const denarioController = require('../controllers/denario');
 const auth = require('../middleware/jwt');
 
 router.post('/simulate_credit', denarioController.simulateCredit);
+router.post('/save_credit', denarioController.saveCredit);
+router.post('/validate_code_phone', denarioController.validateCodePhone);
+router.post('/validate_code_email', denarioController.validateCodeEmail);
 /* router.post('/credit_limit', denarioController.credit_limit); */
 
 module.exports = router;
