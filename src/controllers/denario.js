ac3bb0877ce50a8e99605e148e007151b672731e
Sebastiang29 <sebastian.gomez10@gmail.com>
2024-03-30 22:01:40 -0500

    update: servicios códigos

--- src/controllers/denario.js
+++ src/controllers/denario.js
@@ -10,26 +10,36 @@ const authentication = { url: 'http://localhost:4370/api/', aut: 'AutWanderlustP
 } */
 exports.simulateCredit = async (req, res) => {
     const data = req.body;
-    const resOk = await procesosDenario('wanderlust/simular_solicitud', 'POST', { cedula: data.Document, monto: data.Amount});
+    const resOk = await procesosDenario('wanderlust/simular_solicitud', 'POST', { cedula: data.Document, monto: data.Amount });
     resOk.status == true ? res.status(200).json({ error: false, data: resOk.body }) : res.status(400).json({ error: true, data: resOk.body });
 }
 exports.saveCredit = async (req, res) => {
     const data = req.body;
-    const resOk = await procesosDenario('wanderlust/aprobar_solicitud', 'POST', { cedula: data.Document, monto: data.Amount, plazo: data.Term});
+    const resOk = await procesosDenario('wanderlust/aprobar_solicitud', 'POST', { cedula: data.Document, monto: data.Amount, plazo: data.Term });
     resOk.status == true ? res.status(200).json({ error: false, body: resOk.body, detail: resOk.det }) : res.status(400).json({ error: true, data: resOk.body });
 }
 exports.validateCodePhone = async (req, res) => {
     const data = req.body;
-    const resOk = await procesosDenario('wanderlust/solicitar_cod_sms', 'POST', { cedula: data.Document, token: data.Code});
+    const resOk = await procesosDenario('wanderlust/verificar_cod_sms', 'POST', { cedula: data.Document, token: data.Code });
     resOk.status == true ? res.status(200).json({ error: false }) : res.status(400).json({ error: true });
 }
 exports.validateCodeEmail = async (req, res) => {
     const data = req.body;
-    const resOk = await procesosDenario('wanderlust/verificar_cod_email', 'POST', { cedula: data.Document, numSol: data.NumSol, token: data.Code});
+    const resOk = await procesosDenario('wanderlust/verificar_cod_email', 'POST', { cedula: data.Document, numSol: data.NumSol, token: data.Code });
     resOk.status == 1 ? res.status(200).json({ error: false }) : res.status(400).json({ error: true });
 }
-async function creditLimit (Id){
-    const resOk = await procesosDenario('wanderlust/consultar_cupo', 'POST', { cedula: Id});
+exports.askCodePhone = async (req, res) => {
+    const data = req.body;
+    const resOk = await procesosDenario('wanderlust/solicitar_cod_sms', 'POST', { cedula: data.Document });
+    resOk.status == 1 ? res.status(200).json({ error: false }) : res.status(400).json({ error: true });
+}
+exports.askCodeEmail = async (req, res) => {
+    const data = req.body;
+    const resOk = await procesosDenario('wanderlust/solicitar_cod_email', 'POST', { cedula: data.Document, numSol: data.NumSol });
+    resOk.status == 1 ? res.status(200).json({ error: false }) : res.status(400).json({ error: true });
+}
+async function creditLimit(Id) {
+    const resOk = await procesosDenario('wanderlust/consultar_cupo', 'POST', { cedula: Id });
     return resOk;
 }
 module.exports.creditLimit = creditLimit;
--- src/routes/denario.js
+++ src/routes/denario.js
@@ -5,6 +5,8 @@ const auth = require('../middleware/jwt');
 
 router.post('/simulate_credit', denarioController.simulateCredit);
 router.post('/save_credit', denarioController.saveCredit);
+router.post('/ask_code_phone', denarioController.askCodePhone);
+router.post('/ask_code_email', denarioController.askCodeEmail);
 router.post('/validate_code_phone', denarioController.validateCodePhone);
 router.post('/validate_code_email', denarioController.validateCodeEmail);
 /* router.post('/credit_limit', denarioController.credit_limit); */
