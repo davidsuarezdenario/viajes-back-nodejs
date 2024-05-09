0bea6d780ae6b30bab05e78f0c92819254d566fc
Sebastian <sebastian.gomez@denario.com.co>
2024-05-09 17:57:08 -0500

    update: header v3

--- src/controllers/headerAmadeus.js
+++ src/controllers/headerAmadeus.js
@@ -1,7 +1,6 @@
-const { application } = require("express");
-
 const amadeus = { office_id: 'BOGZ122AR', ws_user: 'WSWPSWAN', password: 'Bj2=yu3kX5zh', company: 'WPS', application: 'WAN' };
 const base64Chars = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/');
+const keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 const END_OF_INPUT = -1;
 
 /* Generate Message ID */
@@ -48,7 +47,7 @@ function getNonce() {
         return result;
     }
     function randomString2(len) {
-        let text = ""; const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; for (var i = 0; i < len; i++)text += possible.charAt(Math.floor(Math.random() * possible.length));
+        let text = ""; const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; for (let i = 0; i < len; i++)text += possible.charAt(Math.floor(Math.random() * possible.length));
         return text;
     }
     return encodeBase64(randomString2(8));
@@ -70,16 +69,187 @@ function getT() {
 
 /* Generate Encrypt */
 function WbsPassword(pwd, timestamp, nonceB64) {
-    var aPwd = parseHexaBytes(SHA1(pwd));
-
-    var aNonce = decode64Bytes(nonceB64);//alert(aNonce);
-    var aTime = stringToArray(timestamp);
-    var aHash = SHA1Bytes(aNonce.concat(aTime.concat(aPwd)));//alert(aHash);
-    var HshPwd = encode64Bytes(parseHexaBytes(aHash));//alert(HshPwd);
-
-    return HshPwd;
-}
-function submitEncrypt(pwd, timestamp, nonceB64) {
+    function rotate_left(n, s) {
+        return (n << s) | (n >>> (32 - s));
+    }
+    function cvt_hex(val) {
+        let str = "", i, v;
+        for (i = 7; i >= 0; i--) {
+            v = (val >>> (i * 4)) & 0x0f;
+            str += v.toString(16);
+        }
+        return str;
+    }
+    function SHA1(msg) {
+        function Utf8Encode(string) {
+            string = string.replace(/\r\n/g, "\n");
+            let utftext = "";
+            for (let n = 0; n < string.length; n++) {
+                const c = string.charCodeAt(n);
+                if (c < 128) {
+                    utftext += String.fromCharCode(c);
+                }
+                else if ((c > 127) && (c < 2048)) {
+                    utftext += String.fromCharCode((c >> 6) | 192); utftext += String.fromCharCode((c & 63) | 128);
+                }
+                else {
+                    utftext += String.fromCharCode((c >> 12) | 224); utftext += String.fromCharCode(((c >> 6) & 63) | 128); utftext += String.fromCharCode((c & 63) | 128);
+                }
+            }
+            return utftext;
+        };
+        const W = new Array(80);
+        let temp, blockstart, i, j, A, B, C, D, E, H0 = 0x67452301, H1 = 0xEFCDAB89, H2 = 0x98BADCFE, H3 = 0x10325476, H4 = 0xC3D2E1F0;
+        msg = Utf8Encode(msg);
+        const msg_len = msg.length;
+        let word_array = new Array();
+        for (i = 0; i < msg_len - 3; i += 4) {
+            j = msg.charCodeAt(i) << 24 | msg.charCodeAt(i + 1) << 16 | msg.charCodeAt(i + 2) << 8 | msg.charCodeAt(i + 3);
+            word_array.push(j);
+        }
+        switch (msg_len % 4) {
+            case 0:
+                i = 0x080000000;
+                break;
+            case 1:
+                i = msg.charCodeAt(msg_len - 1) << 24 | 0x0800000;
+                break;
+            case 2:
+                i = msg.charCodeAt(msg_len - 2) << 24 | msg.charCodeAt(msg_len - 1) << 16 | 0x08000;
+                break;
+            case 3:
+                i = msg.charCodeAt(msg_len - 3) << 24 | msg.charCodeAt(msg_len - 2) << 16 | msg.charCodeAt(msg_len - 1) << 8 | 0x80;
+                break;
+        }
+        word_array.push(i);
+        while ((word_array.length % 16) != 14) word_array.push(0);
+        word_array.push(msg_len >>> 29);
+        word_array.push((msg_len << 3) & 0x0ffffffff);
+        for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
+            for (i = 0; i < 16; i++) W[i] = word_array[blockstart + i];
+            for (i = 16; i <= 79; i++) W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+            A = H0; B = H1; C = H2; D = H3; E = H4;
+            for (i = 0; i <= 19; i++) {
+                temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+                E = D; D = C; C = rotate_left(B, 30); B = A; A = temp;
+            }
+            for (i = 20; i <= 39; i++) {
+                temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+                E = D; D = C; C = rotate_left(B, 30); B = A; A = temp;
+            }
+            for (i = 40; i <= 59; i++) {
+                temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+                E = D; D = C; C = rotate_left(B, 30); B = A; A = temp;
+            }
+            for (i = 60; i <= 79; i++) {
+                temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+                E = D; D = C; C = rotate_left(B, 30); B = A; A = temp;
+            }
+            H0 = (H0 + A) & 0x0ffffffff; H1 = (H1 + B) & 0x0ffffffff; H2 = (H2 + C) & 0x0ffffffff; H3 = (H3 + D) & 0x0ffffffff; H4 = (H4 + E) & 0x0ffffffff;
+        }
+        const temp1 = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+        return temp1.toUpperCase();
+    }
+    function parseHexaBytes(iText) {
+        let aResult = [];
+        for (var i = 0; i < iText.length; i = i + 2) {
+            const aValue = parseInt(iText.substr(i, 2), 16);
+            if (aValue > 255) alert('Too large!');
+            if (aValue == 0) alert('Null value!');
+            aResult.push(aValue);
+        }
+        return aResult;
+    }
+    function decode64Bytes(input) {
+        let output = [], chr1, chr2, chr3, enc1, enc2, enc3, enc4, i = 0;
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+        while (i < input.length) {
+            enc1 = keyStr.indexOf(input.charAt(i++));
+            enc2 = keyStr.indexOf(input.charAt(i++));
+            enc3 = keyStr.indexOf(input.charAt(i++));
+            enc4 = keyStr.indexOf(input.charAt(i++));
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+            output.push(chr1);
+            if (enc3 != 64) {
+                output.push(chr2);
+            }
+            if (enc4 != 64) {
+                output.push(chr3);
+            }
+        }
+        return output;
+    }
+    function stringToArray(iText) {
+        let aResult = [];
+        for (let i = 0; i < iText.length; i = i + 1) {
+            aResult.push(iText.charCodeAt(i));
+        }
+        return aResult;
+    }
+    function SHA1Bytes(msg) {
+        let blockstart, i, j, A, B, C, D, E, temp, H0 = 0x67452301, H1 = 0xEFCDAB89, H2 = 0x98BADCFE, H3 = 0x10325476, H4 = 0xC3D2E1F0;
+        let W = new Array(80), word_array = new Array();
+        const msg_len = msg.length;
+        for (i = 0; i < msg_len - 3; i += 4) {
+            if (msg[i] > 255 || msg[i + 1] > 255 || msg[i + 2] > 255 || msg[i + 3] > 255) alert('Not a byte!');
+            j = msg[i] << 24 | msg[i + 1] << 16 | msg[i + 2] << 8 | msg[i + 3];
+            word_array.push(j);
+        }
+        switch (msg_len % 4) {
+            case 0: i = 0x080000000;
+                break;
+            case 1: i = msg[msg_len - 1] << 24 | 0x0800000;
+                break;
+            case 2: i = msg[msg_len - 2] << 24 | msg[msg_len - 1] << 16 | 0x08000;
+                break;
+            case 3: i = msg[msg_len - 3] << 24 | msg[msg_len - 2] << 16 | msg[msg_len - 1] << 8 | 0x80;
+                break;
+        }
+        word_array.push(i);
+        while ((word_array.length % 16) != 14) word_array.push(0);
+        word_array.push(msg_len >>> 29);
+        word_array.push((msg_len << 3) & 0x0ffffffff);
+        for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
+            for (i = 0; i < 16; i++) W[i] = word_array[blockstart + i];
+            for (i = 16; i <= 79; i++) W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+            A = H0; B = H1; C = H2; D = H3; E = H4;
+            for (i = 0; i <= 19; i++) {
+                temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+                E = D; D = C; C = rotate_left(B, 30); B = A; A = temp;
+            }
+            for (i = 20; i <= 39; i++) {
+                temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+                E = D; D = C; C = rotate_left(B, 30); B = A; A = temp;
+            }
+            for (i = 40; i <= 59; i++) {
+                temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+                E = D; D = C; C = rotate_left(B, 30); B = A; A = temp;
+            }
+            for (i = 60; i <= 79; i++) {
+                temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+                E = D; D = C; C = rotate_left(B, 30); B = A; A = temp;
+            }
+            H0 = (H0 + A) & 0x0ffffffff; H1 = (H1 + B) & 0x0ffffffff; H2 = (H2 + C) & 0x0ffffffff; H3 = (H3 + D) & 0x0ffffffff; H4 = (H4 + E) & 0x0ffffffff;
+        }
+        const temp1 = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+        return temp1.toUpperCase();
+    }
+    function encode64Bytes(input) {
+        let output = '', chr1, chr2, chr3, enc1, enc2, enc3, enc4, i = 0;
+        while (i < input.length) {
+            chr1 = input[i++]; chr2 = input[i++]; chr3 = input[i++]; enc1 = chr1 >> 2; enc2 = ((chr1 & 3) << 4) | (chr2 >> 4); enc3 = ((chr2 & 15) << 2) | (chr3 >> 6); enc4 = chr3 & 63;
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            }
+            else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+            output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
+        }
+        return output;
+    }
     const aPwd = parseHexaBytes(SHA1(pwd));
     const aNonce = decode64Bytes(nonceB64);
     const aTime = stringToArray(timestamp);
@@ -94,12 +264,12 @@ function generateHeader() {
     const mid = getMid();
     const nonce = getNonce();
     const timestamp = getT();
-    submitEncrypt(amadeus.password, timestamp, nonce);
+    const digest = WbsPassword(amadeus.password, timestamp, nonce);
     return {
         message_id: mid,
         nonce: nonce,
         timestamp: timestamp,
-        digest: '',
+        digest: digest,
         company: amadeus.company,
         application: amadeus.application
     };
